52d6814e35688fe84409c2a272f3c8247d9678ed
Bibhanshu Mishra <bibhu0404@gmail.com>
2026-02-20 18:59:00 +0530

    Add files via upload

---
+++ LLD Projects/java/src/chessgame/ChessGameDemo.java
@@ -0,0 +1,9 @@
+package chessgame;
+
+public class ChessGameDemo {
+    public static void main(String[] args) {
+        ChessGame chessGame = new ChessGame();
+        chessGame.setPlayers("Alice", "Bob");
+        chessGame.start();
+    }
+}
